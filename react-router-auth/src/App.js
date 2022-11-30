791817ddda8615deecf179e1b39ecdaf54827d64
DanielNdong <prowebmaking@outlook.es>
2022-11-30 16:58:30 +0100

    5 files modified, 4 added

--- react-router-auth/src/App.js
+++ react-router-auth/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+//Rutas
+import Profile from './views/Profile'
+import Login from './views/Login'
+import Logout from './views/Logout'
+import Home from './views/Home'
 
 function App() {
   return (
-    <div className="App">
-        eeey
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/profile" element={<Profile />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/logout" element={<Logout />}/>
+            <Route path="/" element={<Home />}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
