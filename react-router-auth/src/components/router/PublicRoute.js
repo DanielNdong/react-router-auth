1c88ce7d3f9ae6b73f3821f98a662566143f432c
DanielNdong <prowebmaking@outlook.es>
2022-12-02 09:49:43 +0100

    2 files modified

--- react-router-auth/src/components/router/PublicRoute.js
+++ react-router-auth/src/components/router/PublicRoute.js
@@ -1,24 +1,25 @@
-import {useAuthContext} from '../../contexts/authContext'
-import {PRIVATE, LOGIN, LOGOUT} from '../../config/router/paths'
-import {Navigate, Outlet} from 'react-router'
-import {Link} from 'react-router-dom'
+import { useAuthContext } from '../../contexts/authContext';
+import { PRIVATE, LOGIN, LOGOUT } from '../../config/router/paths';
+import { Navigate, Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
 
-export default function PublicRoute(){
-  const {isAuthenticated} = useAuthContext()
+export default function PublicRoute() {
+  const { isAuthenticated } = useAuthContext();
 
-
-  if(isAuthenticated){
-    return <Navigate to={PRIVATE} />
+  if (isAuthenticated) {
+    return <Navigate to={PRIVATE} />;
   }
 
-  return(
+  return (
     <div>
       <nav>
         <ul>
-          <li><Link to={LOGOUT}>Inicio</Link></li>
+          <li>
+            <Link to={LOGOUT}>Iniciar sesion</Link>
+          </li>
         </ul>
       </nav>
       <Outlet />
     </div>
-  )
+  );
 }
--- react-router-auth/src/views/Logout.js
+++ react-router-auth/src/views/Logout.js
@@ -1,7 +1,12 @@
-export default function Logout(){
-  return(
-    <div>
-      Logout
-    </div>
-  )
+import { useAuthContext } from '../contexts/authContext';
+import { useEffect } from 'react';
+
+export default function Logout() {
+  const { logout } = useAuthContext();
+
+  useEffect(() => {
+    logout();
+  }, [logout]);
+  
+  return <div>Logout</div>;
 }
